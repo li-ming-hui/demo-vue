0a6c2a53d9c39a34fedf1cee1d211e3f62f4c237
li-ming-hui <358166192@qq.com>
2018-04-10 09:22:03 +0800

    完成CRUD功能

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -1,19 +1,26 @@
 <template>
-  <div class="col-sm-3 col-md-2 sidebar">
+ <div class="col-sm-3 col-md-2 sidebar">
     <ul class="nav nav-sidebar">
-      <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-      <li><a href="#">Reports</a></li>
-      <li><a href="#">Analytics</a></li>
-      <li><a href="#">Export</a></li>
+      <router-link tag="li" exact to="/">
+        <a>首页</a>
+      </router-link>
+      <router-link tag="li" to="/hero">
+        <a>英雄管理</a>
+      </router-link>
     </ul>
   </div>
 </template>
+
 <script>
-export default {
-  data() {
-    return {}
-  }
-}
+ export default {
+   data () {
+     return {
+     }
+   }
+ }
 </script>
+
 <style>
+
+ 
 </style>
